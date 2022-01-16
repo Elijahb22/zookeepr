c8e0d34f572963c2507b283ddb337be37380eff5
elijahb22 <elibusick11@gmail.com>
2022-01-16 02:55:01 -0500

    attached frontend to backend

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals.json')
+
+app.use(express.static('public'));
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data 
@@ -101,6 +103,18 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal)
     }
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/animals', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
